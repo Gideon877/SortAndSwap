d3f81332729bdeedd4bfc653a6fe0227bdbb5848
Thabang Gideon Magaola <gideon877@live.com>
2019-05-28 12:53:27 +0200

    fixed failing tests

--- src/main/java/co/projectcodex/sortandswap/SwapMe.java
+++ src/main/java/co/projectcodex/sortandswap/SwapMe.java
@@ -3,15 +3,40 @@ package co.projectcodex.sortandswap;
 public class SwapMe {
 
     public static String[] swapFirstWithLast(String[] values) {
-        return null;
+        int index = values.length - 1;
+        String first = values[0];
+        String last = values[index];
+
+        values[0] = last;
+        values[index] = first;
+
+        return values;
     }
 
     public static String[] swapFirstWithSecond(String[] values) {
-        return null;
+        String first = values[0];
+        String second = values[1];
+        values[0] = second;
+        values[1] = first;
+        return values;
     }
 
     public static int[] swapNeighbours(int[] numbers) {
-        return null;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if(i+1 < numbers.length) {
+                int current = numbers[i];
+                int nextCurrent = numbers[i+1];
+
+                if(current > nextCurrent) {
+                    numbers[i] = nextCurrent;
+                    numbers[i+1] = current;
+
+                }
+            }
+        }
+
+        return numbers;
     }
 
 
